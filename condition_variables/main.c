6541cafaec4fc146b7d93d63a84b9292cb03b2ba
mivchal333 <mivchal@gmail.com>
2020-06-09 16:21:39 +0200

    makefile problem

--- condition_variables/main.c
+++ condition_variables/main.c
@@ -1,19 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <semaphore.h>
 #include <pthread.h>
 #include <unistd.h>
 #include <stdbool.h>
 #include <getopt.h>
 #include "../functions.h"
 #include <unistd.h>
-
-struct Lista
-{
-    int nr_klienta;
-    struct Lista *next;
-};
-
 
 pthread_cond_t wolne_miejsca; //miejsca dostepne w poczekalni
 pthread_cond_t wolny_fryzjer; //fryzjer nie scina w danym momencie wlosow
@@ -24,7 +16,6 @@ pthread_mutex_t fotel; //fotel u fryzjera
 pthread_mutex_t poczekalnia; //mutex do ochrony miejsc w poczekalni
 pthread_mutex_t koniec_klient; //klient po zakonczeniu scinania
 pthread_mutex_t stan_fryzjera; //chroni stan pracy/spania fryzjera
-bool skonczone=false; //flaga ustawiana do sprawdzenia czy wszyscy zostali scieci
 bool czy_fotel_zajety = false; //czy ktos siedzi na fotelu
 bool czy_fryzjer_spi; //czy fryzjer ucina sobie drzemke w oczekiwaniu na klienta
 
@@ -36,49 +27,44 @@ int czas_klienta=10;     // czas w jakim klient przychodzi do salonu od rozpocze
 bool debug=false;       // opcja pozwalajaca na wypisanie list
 int akt_klient=-1; // zmienna przechowujaca aktualnego klienta na fotelu; -1 gdy nie ma klienta
 
-void czekaj(int sec) //funkcja do losowego czekania
-{
-    int x = (rand()%sec) * (rand()%1000000) + 1000000;
-    usleep(x);
-}
-
-struct Lista *odrzuceni = NULL; //lista tych, ktorzy sie nie dostali do poczekalni
-struct Lista *czekajacy = NULL; //lista/kolejka osob czekajacych w poczekalni
+struct Node *clients = NULL;
+struct Node *odrzuceni = NULL; //lista tych, ktorzy sie nie dostali do poczekalni
+struct Node *czekajacy = NULL; //lista/kolejka osob czekajacych w poczekalni
 
 void Wypisz_odrzuceni()
 {
-    struct Lista *temp = odrzuceni;
+    struct Node *temp = odrzuceni;
     printf("Nie weszli: ");
     while(temp!=NULL)
     {
-        printf("%d ",temp->nr_klienta);
+        printf("%d ",temp->id);
         temp = temp->next;
     }
     printf("\n");
 }
 void Wypisz_czekajacy()
 {
-    struct Lista *temp = czekajacy;
+    struct Node *temp = czekajacy;
     printf("Czekaja : ");
     while(temp!=NULL)
     {
-        printf("%d " ,temp->nr_klienta);
+        printf("%d " ,temp->id);
         temp = temp->next;
     }
     printf("\n");
 }
 void Umiesc_odrzuceni(int x)
 {
-    struct Lista *temp = (struct Lista*)malloc(sizeof(struct Lista));
-    temp->nr_klienta = x;
+    struct Node *temp = (struct Node*)malloc(sizeof(struct Node));
+    temp->id = x;
     temp->next = odrzuceni;
     odrzuceni = temp;
     Wypisz_odrzuceni();
 }
 void Umiesc_czekajacy(int x)
 {
-    struct Lista *temp = (struct Lista*)malloc(sizeof(struct Lista));
-    temp->nr_klienta = x;
+    struct Node *temp = (struct Node*)malloc(sizeof(struct Node));
+    temp->id = x;
     temp->next = czekajacy;
     czekajacy = temp;
 
@@ -86,16 +72,16 @@ void Umiesc_czekajacy(int x)
 }
 int pierwszy_kolejce()
 {
-    struct Lista *temp = czekajacy;
+    struct Node *temp = czekajacy;
     while(temp->next!=NULL)
     {
         temp=temp->next;
     }
-    return temp->nr_klienta;
+    return temp->id;
 }
 void usun_ostatni()
 {
-    struct Lista *temp = czekajacy;
+    struct Node *temp = czekajacy;
     while(temp->next->next!=NULL)
     {
         temp=temp->next;
@@ -105,13 +91,13 @@ void usun_ostatni()
 }
 void usun_klienta(int x)
 {
-    struct Lista *temp = czekajacy;
-    struct Lista *pop = czekajacy;
+    struct Node *temp = czekajacy;
+    struct Node *pop = czekajacy;
     while (temp!=NULL)
     {
-        if(temp->nr_klienta==x)
+        if(temp->id==x)
         {
-            if(temp->nr_klienta==czekajacy->nr_klienta)
+            if(temp->id==czekajacy->id)
             {
                 czekajacy=czekajacy->next;
                 free(temp);
@@ -130,16 +116,21 @@ void usun_klienta(int x)
 
 }
 
-void *Klient(void *nr_klienta)
+void *Klient(void *client)
 {
-    czekaj(czas_klienta); //czekamy na przyjscie klienta
-    int nr = *(int *)nr_klienta;
+    struct Node *actualClient = (struct Node *) client;
+    int clientId = (*actualClient).id;
+    int clientTime = (*actualClient).time;
+
+
+    travelToBarbershop(clientTime);
+
     pthread_mutex_lock(&poczekalnia); // blokujemy poczekalnie
     if(liczbaMiejsc>0) //jesli sa wolne miejsca, wejdz
     {
         liczbaMiejsc--;
         printf("Res:%d WRomm: %d/%d [in: %d]  -  nowy klient w poczekalni\n",nie_weszli, liczbaKrzesel-liczbaMiejsc, liczbaKrzesel, akt_klient);
-        if(debug==true) Umiesc_czekajacy(nr); //umiesc klienta w kolejnce poczekalni
+        if(debug==true) Umiesc_czekajacy(clientId); //umiesc klienta w kolejnce poczekalni
         pthread_mutex_unlock(&poczekalnia); //odbezpieczamy poczekalnie
 
         //czekamy az fotel sie zwolni
@@ -153,8 +144,8 @@ void *Klient(void *nr_klienta)
         pthread_mutex_lock(&poczekalnia);
         liczbaMiejsc++;
         printf("Res:%d WRomm: %d/%d [in: %d]  -  klient poszedl do fryzjera\n",nie_weszli, liczbaKrzesel-liczbaMiejsc, liczbaKrzesel, akt_klient);
-        akt_klient=nr;
-        if(debug==true) usun_klienta(nr); //usuwamy klienta z kolejki
+        akt_klient=clientId;
+        if(debug==true) usun_klienta(clientId); //usuwamy klienta z kolejki
         printf("Res:%d WRomm: %d/%d [in: %d]  -  klient jest scinany\n",nie_weszli, liczbaKrzesel-liczbaMiejsc, liczbaKrzesel, akt_klient);
         pthread_mutex_unlock(&poczekalnia);
         //pthread_cond_signal(&wolne_miejsca); //wysylamy sygnal ze jest wolne miejsce
@@ -165,7 +156,7 @@ void *Klient(void *nr_klienta)
         //czekaj az scinanie sie skonczy
         pthread_mutex_lock(&koniec_klient);
         pthread_cond_wait(&skonczylem_scinac, &koniec_klient);
-        skonczone=false; //resetowanie zeby nastepny mogl przyjsc
+        isEnd=false; //resetowanie zeby nastepny mogl przyjsc
         pthread_mutex_unlock(&koniec_klient);
 
         //odblokowanie fotelu i danie znac ze fryzjer jest wolny
@@ -179,22 +170,22 @@ void *Klient(void *nr_klienta)
     {
         nie_weszli++; //zwiekszamy licznik osob ktore sie nie dostaly
         printf("Res:%d WRomm: %d/%d [in: %d]  -  klient nie wszedl\n",nie_weszli, liczbaKrzesel-liczbaMiejsc, liczbaKrzesel, akt_klient);
-        if(debug==true) Umiesc_odrzuceni(nr); //umieszczamy na liste osob ktore sie nie dostaly
+        if(debug==true) Umiesc_odrzuceni(clientId); //umieszczamy na liste osob ktore sie nie dostaly
         pthread_mutex_unlock(&poczekalnia); //odblokuj poczekalnie
     }
 
 }
-void *Fryzjer()
+void *BarberThread()
 {
     pthread_mutex_lock(&stan_fryzjera); //lock na stanie fryzjera zeby zapewnic bezpieczenstwo
-    while(!skonczone) //dopoki sa klienci
+    while(!isEnd) //dopoki sa klienci
     {
         //czekaj az fryzjer zostanie wybudzony
         pthread_cond_wait(&obudz_fryzjera, &stan_fryzjera);
         pthread_mutex_unlock(&stan_fryzjera);
-        if(!skonczone) // //jesli jest klient
+        if(!isEnd) // //jesli jest klient
         {
-            czekaj(czas_fryzjera);  // scinamy klienta
+            doBarberWork();  // scinamy klienta
             pthread_cond_signal(&skonczylem_scinac); //koniec
         }
         else //jesli nie, to idz do domu...
@@ -218,7 +209,7 @@ int main(int argc, char *argv[])
     }
     srand(time(NULL));
 
-    int liczbaKlientow =20; //liczba klientow ktorzy sie pojawia
+    int clientCount =20; //liczba klientow ktorzy sie pojawia
     int wybor;
     int kFlag=0;
     int rFlag=0;
@@ -227,7 +218,7 @@ int main(int argc, char *argv[])
         switch(wybor)
         {
         case 'k': //max liczba klientow pobrana jako argument
-            liczbaKlientow=atoi(optarg);
+            clientCount=atoi(optarg);
             kFlag = 1;
             break;
         case 'r': // liczba krzesel w poczekalni
@@ -257,15 +248,19 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
-    pthread_t *klienci_watki = malloc(sizeof(pthread_t)*liczbaKlientow);
-    pthread_t fryzjer_watek;
-
-    int *tablica=malloc(sizeof(int)*liczbaKlientow);
+    pthread_t *clientThreads = malloc(sizeof(pthread_t) * clientCount);
+    pthread_t barberThread;
 
     int i;
-    for (i=0; i<liczbaKlientow; i++)
-    {
-        tablica[i] = i;
+    if (isDebug == 1) printf("Number of Clients: %d\n", clientCount);
+
+    for (i = 0; i < clientCount; i++) {
+        // Wylosowanie czasu, po jakim klient pojawi się u fryzjera
+        int randomTime = rand() % maxClientArriveTime + 1;
+        // dodajemy klienta do listy
+        push(&clients, i, randomTime);
+        // watek dla klienta, przekazujemy wskaznik na danego klienta
+        pthread_create(&clientThreads[i], NULL, ClientThread, (void *) clients);
     }
 
     //inicjalizacja zmiennych warunkowych
@@ -281,24 +276,18 @@ int main(int argc, char *argv[])
     pthread_mutex_init(&stan_fryzjera, NULL);
 
     //tworzymy watek fryzjera
-    pthread_create(&fryzjer_watek, 0, Fryzjer, 0);
-
-    //tworzymy watki klienta
-    for (i=0; i<liczbaKlientow; ++i)
-    {
-        pthread_create(&klienci_watki[i], 0, Klient, (void *)&tablica[i]);
-    }
+    pthread_create(&barberThread, 0, BarberThread, 0);
 
     //czekamy az zostana obsluzeni
-    for (i=0; i<liczbaKlientow; i++)
+    for (i=0; i < clientCount; i++)
     {
-        pthread_join(klienci_watki[i],0);
+        pthread_join(clientThreads[i], 0);
     }
     //ustawiamy flage ze to koniec roboty na dzisiaj
-    skonczone=true;
+    isEnd=1;
     //budzimy fryzjera zeby poszedl do domu
     pthread_cond_signal(&obudz_fryzjera);
-    pthread_join(fryzjer_watek, 0);
+    pthread_join(barberThread, NULL);
 
     //sprzatanie...
     pthread_cond_destroy(&wolne_miejsca);
@@ -313,5 +302,6 @@ int main(int argc, char *argv[])
 
     free(czekajacy);
     free(odrzuceni);
+    free(clients);
     return 0;
 }
